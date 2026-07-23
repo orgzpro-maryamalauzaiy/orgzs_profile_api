4d584c730b8a4188b6048c6c3f54ecf2e2db91a4
MayangAsura <mayang.asura123@gmail.com>
2026-07-24 02:56:31 +0700

    fixing cors: move vercel.json path

--- api/server.js
+++ api/server.js
@@ -44,8 +44,8 @@ const corsOptions = {
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
-app.use(cors())
-// app.use(cors(corsOptions))
+// app.use(cors())
+app.use(cors(corsOptions))
 
 // app.use(cors())
 
